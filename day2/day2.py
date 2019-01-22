152a2e122a744e5cfd560ec8db25afe39c77b38d
Hcupcdown <370188194@qq.com>
2019-01-22 09:09:35 +0800

    简单线性回归

--- day2/day2.py
+++ day2/day2.py
@@ -6,8 +6,25 @@ import matplotlib.pyplot as plt
 dataset = pd.read_csv('studentscores.csv')
 X=dataset.iloc[: , :1].values
 Y=dataset.iloc[ : , 1].values
-print(X);
-print(Y);
+print(X)
+print(Y)
 
 from sklearn.model_selection import train_test_split
-X-train,X_test,Y_train,Y_test=train_test_split(X，Y,test_size=1/4,random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size = 1/4, random_state = 0) 
+#使用简单线性回归模型来训练
+from sklearn.linear_model import LinearRegression
+regressor =LinearRegression()
+regressor=regressor.fit(X_train,Y_train)
+
+#预测测试集训练结果
+Y_pred=regressor.predict(X_test)
+
+#训练集结果可视化
+plt.scatter(X_train,Y_train,color='red')
+plt.plot(X_train,regressor.predict(X_train),color='blue')
+plt.show()
+
+#测试集结果可视化
+plt.scatter(X_test,Y_test,color='red')
+plt.plot(X_test,Y_pred,color='blue')
+plt.show()
